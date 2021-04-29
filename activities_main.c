d39ab78e4eab2eebce2e57a12c11a5f4119867bb
Vinayak-Karalatti <81227238+Vinayak-Karalatti@users.noreply.github.com>
2021-04-29 23:12:57 +0530

    Update activities_main.c

--- activities_main.c
+++ activities_main.c
@@ -1,1 +1,39 @@
+  
+/**
+ * @file activities_main.c
+ * @author Vinayak K ( vinukaralatti@gmail.com)
+ * @brief Heat Seating Application to set Temperature of seat
+ * @version 0.1
+ * @date 2021-04-27 
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+#include "Activity_1.h"
+#include "Activity_2.h"
+#include "Activity_3.h"
+#include "Activity_4.h"
+
+int main(void)
+{
+    uint16_t temp;
+    
+    while(1)
+    {
+        if(LED()==1) //Check if both the switches are pressed
+        {
+            LED_ON();          //Turn LED ON
+            temp=GetADC();     //To Get the ADC value
+            PWM(temp);         //this is to PWM output based on temperature
+		        USARTWrite(temp); //this is for Serial monitor to display Temperature
+        }
+        else  // other cases
+        {
+            LED_OFF();     //Turn LED OFF
+		        _delay_ms(200);
+        }
+
+    }
+    return 0;
+}
